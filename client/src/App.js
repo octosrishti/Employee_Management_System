86ed5d17d61b163f7cfec3ec6baf8c41de213ffb
octosrishti <srishti.21ug3023@iiitranchi.ac.in>
2024-04-22 14:46:07 +0530

    dash

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
-import { useState } from 'react';
-import './App.css';
-import LeftNav from './components/LeftNav/LeftNav';
-import MainSection from './components/MainSection/MainSection';
-import TopNav from './components/TopNav/TopNav';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login.js";
+import SignUp from "./components/SignUp.js";
+import {RequireToken} from './components/Auth.js'
+ 
+import Dashboard from "./components/Dashboard.js";
+import Home from "./components/Home";
+import Employee from "./components/Employee.js";
+import Profile from "./components/Profile.js";
+import AddEmployee from "./components/AddEmployee.js";
+import EditEmployee from './components/EditEmployee.js'
+ 
 function App() {
-  const [employeeId, setEmployeeId] = useState('')
-  console.log(employeeId)
   return (
-    <div className="App">
-      <TopNav/>
-      <LeftNav employeeId={employeeId}/>
-      <MainSection setEmployeeId={setEmployeeId}/>
+    <div className="app">
+        <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+               
+              <Route path='/' element={
+                  <RequireToken>
+                    <Dashboard />
+                  </RequireToken>
+                  }>
+                  <Route path='' element={<Home />}></Route>
+                  <Route path='/employee' element={<Employee />}></Route>
+                  <Route path='/profile' element={<Profile />}></Route>
+                  <Route path='/create' element={<AddEmployee />}></Route>
+                  <Route path='/employeeedit/:id' element={<EditEmployee />}></Route>
+              </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
-
+   
 export default App;
